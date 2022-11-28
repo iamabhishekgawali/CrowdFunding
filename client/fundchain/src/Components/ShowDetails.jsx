7312a1d3edc07562477d98822b7144c77f09e446
Chetan76666 <chetanjain7028@gmail.com>
2022-11-28 18:47:14 +0530

    ShowDetails+sampleImg+css

--- client/fundchain/src/Components/ShowDetails.jsx
+++ client/fundchain/src/Components/ShowDetails.jsx
@@ -1,6 +1,7 @@
-
+import React from "react"
 import { useEffect,useContext } from "react";
 import { WalletContext } from "../Context/WalletContext";
+import "../Styles/Style.css";
 
 export default  function ShowDetails(currElem){
    
@@ -15,10 +16,26 @@ export default  function ShowDetails(currElem){
 
     // Use this 
     console.log(currentCampaignData)
+    
 
     return (
-        <div>
-           
+        <div className="card-container">
+        <div className="card ">
+          <div className="card-body">
+          <h2 className="card-title"> {currentCampaignData[5]}</h2>
+            <span className="card-number card-circle subtle"> {currentCampaignData[4]} </span>
+            <span className="card-author subtle"> {currentCampaignData[5]}</span>
+
+            <span className="card-description subtle">
+              {currentCampaignData[6]}
+            </span>
+          </div>
+          <img src="./images/covidFund.jpg" alt="images" className="card-media" />
         </div>
+        {/* <button type="button" className="btn">Contribute</button> */}
+        <span className="card-tag">CONTRIBUTE</span>
+
+      </div>
+        
     )
 }
